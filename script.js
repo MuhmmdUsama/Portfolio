7b469904db865c69a447e3f14886b1153a2aa443
Muhmmd-Usama <mummdusama@gmail.com>
2022-04-08 01:07:19 +0200

    Complet form validation & add Css style

--- script.js
+++ script.js
@@ -158,9 +158,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // ######## validate contact form ########
 
-const form = document.querySelector('.contact-form')
-const alert = document.getElementById('alert')
-function validate(){
-  form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const emailValue = form.email.value
+const form = document.querySelector('.contact-form');
+const alert = document.getElementById('alert');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailValue = form.email.value;
+  if (emailValue.toLowerCase() !== emailValue) {
+    alert.classList.add('error');
+    alert.innerHTML = 'ALERT!  Your Email Should Be Lowercase';
+  } else {
+    form.submit();
+  }
+});
